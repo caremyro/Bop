7c71afb9bb321efe72ba47232f88d7782d10c01a
caremyro <remy.gouhier.pro@gmail.com>
2026-08-15 04:50:19 +0200

    FIX: bug fixed, duplicated instanced

--- Program.cs
+++ Program.cs
@@ -1,14 +1,35 @@
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Bop;
 
 static class Program
 {
+    private static Mutex? mutex;
+
     [STAThread]
     static void Main()
     {
-        ApplicationConfiguration.Initialize();
-        Application.Run(new BopApplicationContext());
+        mutex = new Mutex(true, "Bop-Audio-Player-Unique-Instance-Key", out bool isNewInstance);
+
+        if (!isNewInstance)
+        {
+            return;
+        }
+
+        try
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new BopApplicationContext());
+        }
+        finally
+        {
+            if (mutex != null)
+            {
+                mutex.ReleaseMutex();
+                mutex.Dispose();
+            }
+        }
     }
 }
